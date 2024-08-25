83852ef6a726da967280d7b0edc359cd51418eed
Tanishq Sharma <tanishq22@iiserb.ac.in>
2024-08-25 17:32:49 +0530

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# SAC
-This is the official repository of SAC website IISER Bhopal
-https://students.iiserb.ac.in/
+# SAC, IISER BHOPAL 
+## <li> ABOUT</li>
+This is the official repository of IISER Bhopal's SAC website.
+The website is live at <a href="https://students.iiserb.ac.in">https://students.iiserb.ac.in</a> as of now.
+# 
+
+## <li><b>DEVELOPERS:</b></li>
+<ul><li>Tanishq Sharma: <i><a href="https://github.com/TanishqSharma2022">@tanishqsharma2022</a></i></li>
+<li>Shailesh Kachhi: <i><a href="https://github.com/Shailesh22290">@shailesh22290</a></li>
+</ul>
+
+
+
---
+++ scripts/fest.js
@@ -0,0 +1,84 @@
+
+
+
+
+
+
+
+
+(function(){
+    var i = 0;
+
+  
+    var images = ['1.jpg', '3.jpg', '4.jpg', '6.jpg','7.jpg']
+    var container = document.querySelector("#fest_bg");
+    
+
+setInterval(function(){
+            if(i == 6){
+                i = 0;
+            }
+
+        container.style = `background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.8)), url('./images/Enthuzia/${images[i]}'); transition: background 1s ease-in-out; background-size: cover;`;
+
+
+
+    i++;
+}, 20000)
+})();
+// function click(f, btn, i){
+//     btn.addEventListener('click', e =>{
+
+        
+//         // console.log(document.getElementsByClassName('imageslider_wrapper')[i]);
+//         document.getElementsByClassName('imageslider_wrapper')[f].style.left = '-' + i*100 + "%";
+//         currentSlide.classList.remove('current-slide');
+//         btn.classList.add("current-slide");
+//         currentSlide = btn;
+//     })
+// }
+
+
+
+
+
+// var noOfSections = 8;
+
+
+// // NAV INDICATORS IMAGE CAROUSEL FOR FESTSS
+
+// const radioContain = document.getElementsByClassName("radio-container");
+
+// for(var f=0; f<noOfSections; f++){
+
+
+// var btns = radioContain[f].querySelectorAll('button');
+// console.log(btns)
+
+// var currentSlide = document.getElementsByClassName("current-slide")[f];
+
+
+// btns.forEach((btn, i) => {
+    
+//     click(f, btn, i);
+
+// });
+
+
+// }
+
+// var x = document.getElementsByClassName("imageslider_wrapper");
+
+// for(var i=0; i<noOfSections; i++){
+
+// // var images = Array.from(track.children);
+// var y = x[i].querySelectorAll('img');
+// var width = y[0].getBoundingClientRect().width;
+// // console.log(btns.children)
+
+// y.forEach((slide, index) => {
+//     slide.style.left = (100*index)+'%';
+// });
+// }
+
+
